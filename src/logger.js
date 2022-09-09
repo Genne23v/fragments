2cbb82ae65c40ed0c777fa77f13cfc0d669cc177
Wonkeun No <zakk23v@hotmail.com>
2022-09-09 17:28:05 -0400

    Remove comments and add missing setting

--- src/logger.js
+++ src/logger.js
@@ -1,8 +1,6 @@
 const options = { level: process.env.LOG_LEVEL || 'info' };
 
-// If we're doing `debug` logging, make the logs easier to read
 if (options.level === 'debug') {
-    // https://github.com/pinojs/pino-pretty
     options.transport = {
         target: 'pino-pretty',
         options: {
@@ -11,6 +9,4 @@ if (options.level === 'debug') {
     };
 }
 
-// Create and export a Pino Logger instance:
-// https://getpino.io/#/docs/api?id=logger
 module.exports = require('pino')(options);
