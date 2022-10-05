b56ec403a0b0fbf24b8c90f1bbfa76e387f4ce8a
Wonkeun No <zakk23v@hotmail.com>
2022-10-05 14:58:23 -0400

    Test async in CI

--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -7,7 +7,7 @@ describe('GET /v1/fragments', () => {
   test('Incorrect credentials are denied', () =>
     request(app).get('/v1/fragments').auth('invalid@email.com', 'incorrect_password').expect(401));
 
-  test('Authenticated users get a fragments array', (done) => {
+  test('Authenticated users get a fragments array', () => {
     request(app)
       .get('/v1/fragments')
       .auth('test1@test.com', 'Test123$')
@@ -15,7 +15,6 @@ describe('GET /v1/fragments', () => {
       .expect((res) => expect(res.body.status).toBe('ok'))
       .expect((res) => expect(Array.isArray(res.body.fragments)).toBe(true))
       .expect((res) => expect(res.headers['location']).not.toBeUndefined())
-      .end(done);
   });
 
   test('GET /v1/fragments returns 404', (done) => {
