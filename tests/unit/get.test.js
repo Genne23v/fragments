a8fee74bd4e4108a3e29c5dc5f1e16aed6cb4383
Wonkeun No <zakk23v@hotmail.com>
2022-10-04 21:47:58 -0400

    CI unit test failure test

--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -7,18 +7,18 @@ describe('GET v1/fragments', () => {
   test('Incorrect credentials are denied', () =>
     request(app).get('/v1/fragments').auth('invalid@email.com', 'incorrect_password').expect(401));
 
-  // test('Authenticated users get a fragments array', async () => {
-  //   const res = await request(app).get('/v1/fragments').auth('test1@test.com', 'Test123$');
+  test('Authenticated users get a fragments array', () => {
+    const res =  request(app).get('/v1/fragments').auth('test1@test.com', 'Test123$');
     
-  //   expect(res.statusCode).toBe(200);
-  //   expect(res.body.status).toBe('ok');
-  //   expect(Array.isArray(res.body.fragments)).toBe(true);
-  //   expect(res.headers['location']).not.toBeUndefined();
-  // });
+    expect(res.statusCode).toBe(200);
+    expect(res.body.status).toBe('ok');
+    expect(Array.isArray(res.body.fragments)).toBe(true);
+    expect(res.headers['location']).not.toBeUndefined();
+  });
 
-  // test('GET /fragments returns 404', async () => {
-  //   const res = await request(app).get('/fragments').auth('test1@test.com', 'Test123$')
-  //   expect(res.statusCode).toBe(404);
-  //   expect(res.body.status).toBe('error');
-  // })
+  test('GET /fragments returns 404', async () => {
+    const res = await request(app).get('/fragments').auth('test1@test.com', 'Test123$')
+    expect(res.statusCode).toBe(404);
+    expect(res.body.status).toBe('error');
+  })
 });
