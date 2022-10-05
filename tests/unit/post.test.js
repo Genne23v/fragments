fb2063296ec24b24a876fecd6f1e895b3a8f71e3
Wonkeun No <zakk23v@hotmail.com>
2022-10-05 14:04:16 -0400

    CI failure test

--- tests/unit/post.test.js
+++ tests/unit/post.test.js
@@ -7,7 +7,7 @@ describe('POST v1/fragments', () => {
   test('Incorrect credentials are denied', () =>
     request(app).post('/v1/fragments').auth('invalid@email.com', 'incorrect_password').expect(401));
 
-  test('Authenticated users with valid content type get a 200 response', (done) => {
+  test('Authenticated users with valid content type get a 200 response', () => {
     request(app)
       .post('/v1/fragments')
       .auth('test1@test.com', 'Test123$')
@@ -15,8 +15,7 @@ describe('POST v1/fragments', () => {
       .send('post test')
       .expect(200)
       .expect((res) => expect(res.body.status).toBe('ok'))
-      .expect(res => expect(res.body.fragment.type).toBe('text/plain; charset=utf-8'))
-      .end(done);
+      .expect((res) => expect(res.body.fragment.type).toBe('text/plain; charset=utf-8'));
   });
 
   test('Authenticated users with invalid content type get a 415 response', (done) => {
