7d58da58e22571b1b79f169c8847683d59469b5a
Wonkeun No <zakk23v@hotmail.com>
2022-11-26 12:45:59 -0500

    Pipeline test

--- src/authorization/index.js
+++ src/authorization/index.js
@@ -1,7 +1,10 @@
+const logger = require('../logger');
+
 if (process.env.AWS_COGNITO_POOL_ID && process.env.AWS_COGNITO_CLIENT_ID) {
   module.exports = require('./cognito');
 } else if (process.env.HTPASSWD_FILE && process.NODE_ENV !== 'production') {
   module.exports = require('./basic-auth');
 } else {
+  logger.debug(process.env.AWS_COGNITO_CLIENT_ID, process.env.AWS_COGNITO_POOL_ID);
   throw new Error('Missing env vars: no authorization configuration found');
 }
